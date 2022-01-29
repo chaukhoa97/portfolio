c777d09daabb1070276d645e589728d002c91652
khoacd-1208 <chau.dinh.khoa@sun-asterisk.com>
2022-01-29 22:00:01 +0700

    refactor readme

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   headerTitle: "",
   description: `When I was first starting to learn programming, I used to note all my should-remember things in one of my repositories, sometimes in documents or anything that can be written on.
 
-  As my coding journey goes on they have started to become unmanageable and it messes up when I try to find something, so I come up with the idea "Why don't I keep them all in a hybrid portfolio-storage-blog personal page?", and here it is.
+  As my coding journey goes on they have started to become unmanageable, and it messes up when I try to find something, so I come up with the idea "Why don't I keep them all in a hybrid portfolio-storage-blog personal page?", and here it is.
 
   Although the page is almost things I write for myself, I hope you will find something in your interest when spending time here.
   
